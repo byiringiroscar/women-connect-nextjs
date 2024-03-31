43d6ce8423ffa5883df8885d2018979a713af5a2
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-31 16:45:41 +0200

    wip on nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,22 +5,28 @@ import { NAV_LINKS } from '@/constants'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between padding-container items-center py-4 z-50 xl:px-28'>
+    <nav className='flex justify-between padding-container items-center py-6 z-50 xl:px-28'>
         <Link href="/" className='flex gap-1 items-center'>
           <FaShieldHeart className='p-1 bg-yellow-500 rounded-sm'  />
-          <span className=''>Connect teams</span> 
+          <span className='montserrat-700 font-bold'>Connect teams</span> 
         </Link>
-        <ul className='hidden h-full gap-12 lg:flex'>
+        <ul className='hidden h-full gap-4 lg:flex'>
             {NAV_LINKS.map((link) => (
                 <Link
                 href={link.href}
                 key={link.key}
-                className=''
+                className='montserrat-400 text-black'
             >
                 {link.label}
             </Link>
             ))}
+            <button
+        className='hidden lg:flex bg-[#CDEC2E] text-black  px-6 opensans-font-regular text-[14px] py-[2px] button-style relative'>
+            <div className='layer-button'></div>
+            Login
+        </button>
         </ul>
+        
     </nav>
   )
 }
