b2e0336ed4f6cc71708c6b5e8de039f6cca8566e
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-31 15:57:37 +0200

    wip on nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { FaShieldHeart } from "react-icons/fa6";
 import Link from "next/link";
+import { NAV_LINKS } from '@/constants'
 
 const Navbar = () => {
   return (
-    <nav>
-        <Link href="/">
-          <FaShieldHeart />
+    <nav className='flex justify-between padding-container items-center py-4 z-50 xl:px-28'>
+        <Link href="/" className='flex gap-1 items-center'>
+          <FaShieldHeart className='p-1 bg-yellow-500 rounded-sm'  />
+          <span className=''>Connect teams</span> 
         </Link>
+        <ul className='hidden h-full gap-12 lg:flex'>
+            {NAV_LINKS.map((link) => (
+                <Link
+                href={link.href}
+                key={link.key}
+                className=''
+            >
+                {link.label}
+            </Link>
+            ))}
+        </ul>
     </nav>
   )
 }
