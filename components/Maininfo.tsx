cb55f8551087a2cc2421ac79552b0589a552c815
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-01 01:14:31 +0200

    Update Maininfo.tsx

--- components/Maininfo.tsx
+++ components/Maininfo.tsx
@@ -8,10 +8,10 @@ const Maininfo = () => {
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-5 py-2 pr-1 bg-black'>
         <div className='flex flex-col gap-8 items-start justify-center p-8 bg-black'>
             <h2 className='pacifico-regular text-white text-[40px] leading-[50px]'>Community Insights</h2>
-            <p className='opensans-font-500 leading-[15px] text-[15px] text-white'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis error minima reiciendis Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis error minima reiciendis Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis error minima reiciendis.</p>
-            <p className='opensans-font-500 leading-[15px] text-[15px] text-white'>The library is in the public domain under the CC0 License. This means you can copy, modify, distribute, remix, burn, and use the work, even for commercial purposes, without asking permission.</p>
+            <p className='opensans-font-500 leading-[18px] text-[15px] text-gray-300'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis error minima reiciendis Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis error minima reiciendis Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis error minima reiciendis.</p>
+            <p className='opensans-font-500 leading-[18px] text-[15px] text-gray-300'>The library is in the public domain under the CC0 License. This means you can copy, modify, distribute, remix, burn, and use the work, even for commercial purposes, without asking permission.</p>
             
-            <Link href="/" className='underline montserrat-700 text-white'>
+            <Link href="/" className='underline montserrat-700 text-white hover:text-[#9b9844] transition-colors duration-200 ease-in-out'>
                 Connect with us
             </Link>
 
