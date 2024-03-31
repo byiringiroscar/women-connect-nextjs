1557eb8723961062f647288137bcfeb1bf1bdadc
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-31 15:45:27 +0200

    wip on nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { FaShieldHeart } from "react-icons/fa6";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+        <Link href="/">
+          <FaShieldHeart />
+        </Link>
+    </nav>
   )
 }
 
---
+++ constants/index.ts
@@ -0,0 +1,6 @@
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'deveopers', label: 'Developers' },
+    { href: '/', key: 'organizations', label: 'Organizations' },
+    { href: '/', key: 'contact', label: 'Contact' },
+  ];
