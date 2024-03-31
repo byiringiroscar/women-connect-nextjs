8a7121ef125056d19be8e7fa6a50e6e60b489f6f
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-03-31 23:00:13 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { FaFacebook } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaTwitter } from "react-icons/fa";
+import { FaDribbbleSquare } from "react-icons/fa";
+import Link from 'next/link';
+
+
 
 const Footer = () => {
+// get current year
+const date = new Date();
+const year = date.getFullYear();
   return (
-    <div className=''>Footer</div>
+    <div className='padding-container py-10 flex flex-col lg:flex-row justify-between items-center'>
+        <div className='flex gap-3 justify-center items-center'>
+            <FaFacebook className=' text-[15px]' />
+            <RiInstagramFill className=' text-[15px]' />
+            <FaTwitter className=' text-[15px]' />
+            <FaDribbbleSquare className=' text-[15px]' />
+            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'>Privacy</Link>
+            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'>Terms</Link>
+            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'>Other Projects</Link>
+            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'>Help center</Link>
+
+
+        </div>
+        <div className='flex gap-3 justify-center items-center'>
+            <p className='montserrat-400 text-[16px] font-bold text-gray-800'>© All rights reserved {year}</p>
+            </div>
+    </div>
   )
 }
 
