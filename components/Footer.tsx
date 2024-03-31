247944843ed5cdb316b45bb00c2f589306b9e1ad
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-01 01:11:55 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
 const date = new Date();
 const year = date.getFullYear();
   return (
-    <div className='padding-container py-10 flex flex-col lg:flex-row justify-between items-center'>
+    <div className='padding-container py-10 flex flex-col gap-4 lg:flex-row justify-between items-center'>
         <div className='flex gap-3 justify-center items-center'>
             <FaFacebook className=' text-[15px]' />
             <RiInstagramFill className=' text-[15px]' />
@@ -20,8 +20,7 @@ const year = date.getFullYear();
             <FaDribbbleSquare className=' text-[15px]' />
             <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'>Privacy</Link>
             <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'>Terms</Link>
-            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'>Other Projects</Link>
-            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'>Help center</Link>
+            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'> Projects</Link>
 
 
         </div>
