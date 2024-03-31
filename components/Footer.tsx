6c3fed29b9c80f71a258a3c1c331cb972349b033
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-01 01:15:56 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,18 +14,18 @@ const year = date.getFullYear();
   return (
     <div className='padding-container py-10 flex flex-col gap-4 lg:flex-row justify-between items-center'>
         <div className='flex gap-3 justify-center items-center'>
-            <FaFacebook className=' text-[15px]' />
-            <RiInstagramFill className=' text-[15px]' />
-            <FaTwitter className=' text-[15px]' />
-            <FaDribbbleSquare className=' text-[15px]' />
-            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'>Privacy</Link>
-            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'>Terms</Link>
-            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800'> Projects</Link>
+            <FaFacebook className=' text-[15px] hover:text-[#9b9844] transition-colors duration-200 ease-in-out cursor-pointer' />
+            <RiInstagramFill className=' text-[15px] hover:text-[#9b9844] transition-colors duration-200 ease-in-out cursor-pointer' />
+            <FaTwitter className=' text-[15px] hover:text-[#9b9844] transition-colors duration-200 ease-in-out cursor-pointer' />
+            <FaDribbbleSquare className=' text-[15px] hover:text-[#9b9844] transition-colors duration-200 ease-in-out cursor-pointer' />
+            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800 hover:text-[#9b9844] cursor-pointer transition-colors duration-200 ease-in-out'>Privacy</Link>
+            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800 hover:text-[#9b9844] cursor-pointer transition-colors duration-200 ease-in-out'>Terms</Link>
+            <Link href='/' className='montserrat-400 text-[16px] font-bold text-gray-800 hover:text-[#9b9844] cursor-pointer transition-colors duration-200 ease-in-out'> Projects</Link>
 
 
         </div>
         <div className='flex gap-3 justify-center items-center'>
-            <p className='montserrat-400 text-[16px] font-bold text-gray-800'>© All rights reserved {year}</p>
+            <p className='montserrat-400 text-[16px] font-bold text-gray-800 cursor-pointer'>© All rights reserved {year}</p>
             </div>
     </div>
   )
